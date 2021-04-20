8db96f1fdda9ead1f74fa9f5317f9222886f7222
chensongxianqu <uxnx7573@163.com>
2021-04-21 07:46:49 +0800

    Add sitemaps

--- config/settings/common.py
+++ config/settings/common.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django_bleach',
+    'django.contrib.sitemaps',
 
     # third party apps
     'imagekit',
--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,23 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
+from django.contrib.sitemaps import GenericSitemap
+from django.contrib.sitemaps.views import sitemap
+
+from blog.models import Post
+
+info_dict = {
+    'queryset': Post.objects.all(),
+    'date_field': 'modified',
+}
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^comment/', include('django_comments.urls')),
     url(r'', include('blog.urls', namespace='blog')),
     url(r'^comment/', include('comments.urls', namespace='comments')),
+    url(r'^sitemap\.xml$', sitemap,
+        {'sitemaps': {'blog': GenericSitemap(info_dict, priority=0.6)}},
+        name='django.contrib.sitemaps.views.sitemap'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
